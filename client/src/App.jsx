75fed9bf902ac7005e6366a47c4e66189dd499fe
Volodymyr Kozak <volodymyr.kozak111@gmail.com>
2022-04-25 16:35:07 +0300

    fix issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
-import {BrowserRouter as Router, Routers, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 //import './App.css' //import HomePage from "./HomePage" // and many other pages....
-import HomePage from '../Pages/Home/Home.jsx'
-import AccountPage from '../Pages/Account/Account.jsx'
-import BasketPage from '../Pages/Basket/Basket.jsx'
-import CatalogPage from '../Pages/Catalog/Catalog.jsx'
-import CataloguePage from '../Pages/Catalogue/Catalogue.jsx'
-import ContactsPage from '../Pages/Contacts/Contacts.jsx'
-import CooperationPage from '../Pages/Cooperation/Cooperation.jsx'
-import DeliveryPage from '../Pages/Delivery/Delivery.jsx'
-import LogInOrRegistrationPage from '../Pages/LogInOrRegistration/LogInOrRegistration.jsx'
-import NeedGoodPage from '../Pages/NeedGood/NeedGoods.jsx'
-import NewPage from '../Pages/New/New.jsx'
-import OrderingPage from '../Pages/Ordering/Ordering.jsx'
-import OutfitPage from '../Pages/Outfit/Outfit.jsx'
-import PrivacyPolicyPage from '../Pages/PrivacyPolicy/PrivacyPolicy.jsx'
-import TableOfSizesPage from '../Pages/TableOfSizes/TableOfSizes.jsx'
-import TermsOfSaleOfGoodsPage from '../Pages/TermsOfSaleOfGoods/TermsOfSalesGoods.jsx'
-import VacanciesPage from '../Pages/Vacancies/Vacancies.jsx'
-import ViewOutfitsPage from '../Pages/ViewOutfits/ViewOutfits.jsx'
+import HomePage from './Pages/Home/Home.jsx'
+import AccountPage from './Pages/Account/Account.jsx'
+import BasketPage from './Pages/Basket/Basket.jsx'
+import CatalogPage from './Pages/Catalog/Catalog.jsx'
+import CataloguePage from './Pages/Catalogue/Catalogue.jsx'
+import ContactsPage from './Pages/Contacts/Contacts.jsx'
+import CooperationPage from './Pages/Cooperation/Cooperation.jsx'
+import DeliveryPage from './Pages/Delivery/Delivery.jsx'
+import LogInOrRegistrationPage from './Pages/LogInOrRegistration/LogInOrRegistration.jsx'
+import NeedGoodPage from './Pages/NeedGood/NeedGoods.jsx'
+import NewPage from './Pages/New/New.jsx'
+import OrderingPage from './Pages/Ordering/Ordering.jsx'
+import OutfitPage from './Pages/Outfit/Outfit.jsx'
+import PrivacyPolicyPage from './Pages/PrivacyPolicy/PrivacyPolicy.jsx'
+import TableOfSizesPage from './Pages/TableOfSizes/TableOfSizes.jsx'
+import TermsOfSaleOfGoodsPage from './Pages/TermsOfSaleOfGoods/TermsOfSalesGoods.jsx'
+import VacanciesPage from './Pages/Vacancies/Vacancies.jsx'
+import ViewOutfitsPage from './Pages/ViewOutfits/ViewOutfits.jsx'
 
 function App(){
     return(
         <div className="App">
             <Router>
-                <Routers>
+                <Routes>
                    <Route>
                        <Route index element ={<HomePage />}/>
                        <Route path='/privacyPolicy' element ={<PrivacyPolicyPage />}/>
@@ -45,7 +45,7 @@ function App(){
                        <Route path='/catalogue/:item' element ={<NeedGoodPage />}/>
                        <Route path='/newClothes' element ={<NewPage />}/>   
                    </Route>
-                   </Routers>
+                   </Routes>
                 </Router>
         </div>
     );
--- client/src/Pages/Contacts/Contacts.jsx
+++ client/src/Pages/Contacts/Contacts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-function contacts(){
+function Contacts(){
     return(
         <div>ContactsPage</div>
     )
