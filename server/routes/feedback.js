fae6c98c44ae5752d42607d2fab017682ebe720f
SheamBest <drugnaroduvmire@gmail.com>
2022-05-25 16:14:02 +0300

    create Feedback endpoints

--- server/routes/feedback.js
+++ server/routes/feedback.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {product} = require('../models');
+const {feedback} = require('../models');
 
 const router = express.Router();
 
@@ -10,13 +10,24 @@ router.get('/', async (req,res) => {
 
 router.post('/', async (req,res) => {
     const Feedback = req.body;
-    await Feedback.create(Feedback);
+    await feedback.create(Feedback);
     res.status(200).json(Feedback);
 });
 
-router.post('/', async (req,res) => {
-    const Feedback = req.body;
-    await Feedback.create(Feedback);
-    res.status(200).json(Feedback);
+router.delete(`/byId/:id`, async (req,res) => {
+    const id = req.params.id;
+    if (!id)
+    {
+        res.status(404).json({message: 'There is not that comment'})
+    }
+    const Feedback = await feedback.destroy({
+        where: {ID_Feedback: id}})
+        if (!Feedback) {
+            res.status(464).json({message: 'Failed to delete comment'})
+        }
+        else{
+            res.status(200).json(Feedback);
+        }
 });
+
 module.exports = router;
