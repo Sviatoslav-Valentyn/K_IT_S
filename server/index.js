291b2c3e68c91ab1d78e9ff5a00a9d021fa60975
SheamBest <drugnaroduvmire@gmail.com>
2022-06-07 14:12:33 +0300

    installing jest and supertest

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 const express = require ('express');
 const cors = require ('cors');
 
-const db = require('./models');
-
-
 const app = express();
 app.use(express.json({ limit: '2mb' }));
 app.use(cors());
@@ -16,8 +13,4 @@ app.use('/user', userRouter);
 app.use('/product', productRouter);
 app.use('/feedback', feedbackRouter);
 
-db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log('server is running');
-    });
-});
+module.exports = app;
